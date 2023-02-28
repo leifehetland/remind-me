92732f046de0212a214ba4772e925c5252052f67
leifehetland <lehetland1@gmail.com>
2023-02-28 16:09:32 -0500

    initial styles and variable declaration

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const reminderTitle = document.querySelector(".note-title");
+const reminderBody = document.querySelector(".note-textarea");
+const saveBtn = document.querySelector(".save-note");
+const newBtn = document.querySelector(".new-btn");
+
+
+function saveReminder(params) {
+    
+}
+
+saveBtn.addEventListener("click", saveReminder)
+
+
+
